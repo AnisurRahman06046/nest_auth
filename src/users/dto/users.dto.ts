6d63669877ac526fd6bb3768077c355e36a07c0d
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-05-15 20:46:16 +0600

    user dto is created

---
+++ src/users/dto/users.dto.ts
@@ -0,0 +1,50 @@
+import {
+  IsString,
+  IsOptional,
+  IsNotEmpty,
+  IsDateString,
+  IsUrl,
+} from 'class-validator';
+
+export class AddressDto {
+  @IsOptional()
+  @IsString()
+  permanent?: string;
+
+  @IsOptional()
+  @IsString()
+  present?: string;
+}
+
+export class UserDto {
+  @IsNotEmpty({ message: 'User name is required' })
+  @IsString()
+  firstName: string;
+
+  @IsNotEmpty({ message: 'Last name is required' })
+  @IsString()
+  lastName: string;
+
+  @IsNotEmpty({ message: 'Password is required' })
+  @IsString()
+  password: string;
+
+  @IsNotEmpty({ message: 'Confirm password is required' })
+  @IsString()
+  confirmPassword: string;
+
+  @IsOptional()
+  @IsString()
+  phoneNumber?: string;
+
+  @IsOptional()
+  address?: AddressDto;
+
+  @IsOptional()
+  @IsDateString()
+  dob?: string;
+
+  @IsOptional()
+  @IsUrl()
+  profileImg?: string;
+}
