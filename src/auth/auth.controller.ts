726dfe517d6c8981c0e59a92a7d500f3a16d691c
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-05-15 21:11:10 +0600

    auth service and controller for signup is done

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UserDto } from 'src/users/dto/users.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+  @Post('signup')
+  async registerUser(@Body() payload: UserDto) {
+    const result = await this.authService.register(payload);
+    return {
+      status: HttpStatus.CREATED,
+      message: 'user is created',
+      data: result,
+    };
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { UserDto } from 'src/users/dto/users.dto';
+import { User } from 'src/users/users.schema';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  //   register user
+  async register(payload: UserDto) {
+    const result = await this.userModel.create(payload);
+    return result;
+  }
+}
