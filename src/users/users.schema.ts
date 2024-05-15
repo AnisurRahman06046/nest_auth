3d1470e3ddd93b1d558ffb12cea8a0812608c8cd
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-05-15 20:41:24 +0600

    user schema is created

---
+++ src/users/users.schema.ts
@@ -0,0 +1,37 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+export class Address {
+  @Prop({ required: false })
+  permanent?: string;
+
+  @Prop({ required: false })
+  present?: string;
+}
+
+@Schema()
+export class User {
+  @Prop({ required: [true, 'User name is required'] })
+  firstName: string;
+
+  @Prop({ required: [true, 'Last name is required'] })
+  lastName: string;
+
+  @Prop({ required: [true, 'Password is required'] })
+  password: string;
+
+  @Prop({ required: [true, 'Confirm password is required'] })
+  confirmPassword: string;
+
+  @Prop({ required: false })
+  phoneNumber?: string;
+
+  @Prop({ required: false })
+  address?: Address;
+
+  @Prop({ required: false })
+  dob?: string;
+  @Prop({ required: false })
+  profileImg?: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
