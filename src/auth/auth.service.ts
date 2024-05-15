3340793436ad25f2dcf68075dd309aea0e2da3d0
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-05-15 22:27:42 +0600

    password hashing is done

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { UserDto } from 'src/users/dto/users.dto';
 import { User } from 'src/users/users.schema';
-
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class AuthService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
   //   register user
   async register(payload: UserDto) {
-    const result = await this.userModel.create(payload);
+    // console.log('before creating')
+    if (payload.password != payload.confirmPassword)
+      throw new HttpException(
+        'Password does not match',
+        HttpStatus.BAD_REQUEST,
+      );
+    const hashedPassword = await bcrypt.hash(payload.password, 10); // 10 is the salt rounds
+
+    // Replace the plain password with the hashed one in the payload
+    const payloadWithHashedPassword: UserDto = {
+      ...payload,
+      password: hashedPassword,
+      confirmPassword: hashedPassword,
+    };
+
+    const result = await this.userModel.create(payloadWithHashedPassword);
+    // console.log('after creating')
     return result;
   }
 }
