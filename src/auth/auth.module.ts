2f34dec846442c6b32f1ec97ab63385db2a6da80
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-05-17 01:23:48 +0600

    rbac is done

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { RolesGuard } from './roles.guard';
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '2m' },
+      signOptions: { expiresIn: '1d' },
     }),
   ],
   controllers: [AuthController],
